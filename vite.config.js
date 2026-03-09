f6c4a34c835e6113faf4bad219bee97cf54fb101
Moorthy Chandiran <moorthy.chandiran21@gmail.com>
2026-03-09 18:57:14 +0530

    frontend sitemap comented

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
-import { sitemapMiddleware } from "./generate-sitemap.js";
+// import { sitemapMiddleware } from "./generate-sitemap.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,12 +12,12 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    {
-      name: "sitemap-middleware",
-      configureServer(server) {
-        server.middlewares.use(sitemapMiddleware());
-      },
-    },
+    // {
+    //   name: "sitemap-middleware",
+    //   configureServer(server) {
+    //     server.middlewares.use(sitemapMiddleware());
+    //   },
+    // },
   ],
   resolve: {
     alias: {
