5471e74c4e7ac321c8c8df4660d38e875beec5be
Moorthy Chandiran <moorthy.chandiran21@gmail.com>
2026-03-11 13:46:30 +0530

    chnages done in the home page

--- src/components/blog/blog-faq.jsx
+++ src/components/blog/blog-faq.jsx
@@ -1,13 +1,10 @@
-
-
-
-
-
 import { Link } from "react-router-dom";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion";
-
-
-
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
 
 const BlogFaq = ({ title = "FAQs", faqs }) => {
   if (!faqs?.length) return null;
@@ -16,33 +13,24 @@ const BlogFaq = ({ title = "FAQs", faqs }) => {
     <section className="py-2">
       <div className="mx-auto max-w-7xl ">
         <div className="grid gap-8 ">
-          
-         
           <div className="md:col-span-2">
-            <h2 className="text-foreground text-4xl font-semibold">
-              {title}
-            </h2>
-
-          
-
-            
+            <h2 className="text-[#0F3652] text-4xl font-semibold">{title}</h2>
           </div>
 
-        
           <div className="md:col-span-3">
             <Accordion type="single" collapsible>
               {faqs.map((item) => (
                 <AccordionItem
                   key={item.id}
                   value={item.id}
-                    className="border-b border-[#F3831C]/20"
+                  className="border-b border-[#F3831C]/20"
                 >
-               <AccordionTrigger className="cursor-pointer text-[#0F3652] font-medium hover:no-underline hover:text-[#F3831C] text-left">
+                  <AccordionTrigger className="cursor-pointer text-[#0F3652] font-medium hover:no-underline hover:text-[#F3831C] text-left">
                     {item.question}
                   </AccordionTrigger>
 
                   <AccordionContent>
-                  <p className="text-[#0F3652] text-base whitespace-pre-line">
+                    <p className="text-[#0F3652] text-base whitespace-pre-line">
                       {item.answer}
                     </p>
                   </AccordionContent>
@@ -50,8 +38,6 @@ const BlogFaq = ({ title = "FAQs", faqs }) => {
               ))}
             </Accordion>
           </div>
-
-          
         </div>
       </div>
     </section>
